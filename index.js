99b65522410c9d04714a94c5bdf70677dfb0a6c7
rajaaghanem <rajaaghanem@gmail.com>
2022-02-02 16:50:21 +0200

    Added savedcomponent schema, REST and functions

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const path = require('path');
 const bcrypt = require ('bcryptjs');
 const jwt = require ("jsonwebtoken");
 
-// const productsRoute = require ('./src/routers/product.route.js')
+const savedComponentsRoute = require ('./src/routers/savedComponent.route.js')
 const usersRoute = require ('./src/routers/user.route.js');
 // const { require } = require("yargs");
 
-// const Product = require("./models/product");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,7 +20,7 @@ app.use(express.static(publicPath));
 
 app.use(express.json());
 app.use(usersRoute);
-// app.use(productsRoute);
+app.use(savedComponentsRoute);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(publicPath, 'index.html'));
