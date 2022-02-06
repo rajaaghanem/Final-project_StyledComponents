987d1660b3c7e999567d73bc214cbdd880e0c761
rajaaghanem <rajaaghanem@gmail.com>
2022-02-06 17:19:07 +0200

    add categories page, style signin and logout components

---
+++ client/src/pages/userProfile/homepage/HomePage.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
+import { AuthProvider, useAuth } from "../../../contexts/AuthContext";
+import Login from "../../../components/login/Login";
+import Signup from "../../../components/signup/Signup";
+import "./homepage.css";
+
+function HomePage() {
+  const [toggle, setToggle] = useState(true);
+  const { currentUser } = useAuth();
+  const history = useHistory();
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  console.log("currentUser in homepage", currentUser);
+
+  return (
+    <div className="homepage-container">
+        <div className="auth-components">
+        {toggle && <Signup handleClick={handleToggle}/>}
+        {!toggle && <Login handleClick={handleToggle}/>}
+        </div>
+    </div>
+  );
+}
+
+export default HomePage;
