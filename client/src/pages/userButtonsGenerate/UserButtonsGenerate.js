b76fcda626db1ad84b58759c7622f5e44df9c5fe
rajaaghanem <rajaaghanem@gmail.com>
2022-02-08 23:08:12 +0200

    add {data:{}} to the axios req

--- client/src/pages/userButtonsGenerate/UserButtonsGenerate.js
+++ client/src/pages/userButtonsGenerate/UserButtonsGenerate.js
@@ -23,7 +23,7 @@ function UserButtonsGenerate(props) {
     const handleComponentById = async () => {
       const id = componentId;
       try {
-        const response = await myApi.get(`/savedcomponents/${componentId}`, {});
+        const response = await myApi.get(`/savedcomponents/${componentId}`, {data:{}});
         console.log(response);
         
         setComponentPropsArray(response.data.propsArr);
