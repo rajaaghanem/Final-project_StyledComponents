4bb1c4dd0168033a1c9e183ec33cf589ca027aae
rajaaghanem <rajaaghanem@gmail.com>
2022-02-08 22:39:11 +0200

    switched the find and get routes

--- src/routers/savedComponent.route.js
+++ src/routers/savedComponent.route.js
@@ -45,6 +45,21 @@ router.get(
   }
 );
 
+
+
+//Get all saved components of the logged in user
+router.get("/api/savedcomponents", auth, async (req, res) => {
+  try {
+    // const savedComponent = await SavedComponent.find({owner: req.user._id});
+    // res.send(savedComponent);
+
+    await req.user.populate("savedComponents");
+    res.send(req.user.savedComponents);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
 //Get savedComponent by id
 router.get("/api/savedcomponents/find/:id", auth, async (req, res) => {
   // const _id = req.params.id;
@@ -65,21 +80,6 @@ router.get("/api/savedcomponents/find/:id", auth, async (req, res) => {
     res.status(500).send(e.message);
   }
 });
-
-
-//Get all saved components of the logged in user
-router.get("/api/savedcomponents", auth, async (req, res) => {
-  try {
-    // const savedComponent = await SavedComponent.find({owner: req.user._id});
-    // res.send(savedComponent);
-
-    await req.user.populate("savedComponents");
-    res.send(req.user.savedComponents);
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
-
 
 //Update a saved copmonent fields
 router.patch("/api/savedcomponents/:id", auth, async (req, res) => {
